329589cc90afba84da38e5bdb5dfb08cc0380c5b
zniii <golshanjimee@gmail.com>
2022-09-21 21:16:56 +0545

    adding moving background and ground

--- main.lua
+++ main.lua
@@ -1,16 +1,32 @@
 push = require'push'
 
+Class = require 'class'
+
+require 'Bird'
+
 WINDOW_WIDTH = 1280
 WINDOW_HEIGHT = 720
 
 VIRTUAL_WIDTH = 512
 VIRTUAL_HEIGHT = 288
 
-local background = love.graphics.newImage('background.png')
-local ground = love.graphics.newImage('ground.png')
+local background = love.graphics.newImage('/images/background.png')
+local backgroundScroll = 0
+
+local ground = love.graphics.newImage('/images/ground.png')
+local groundScroll = 0
+
+--speed of the scroll of the images
+local BACKGROUND_SCROLL_SPEED = 30
+local GROUND_SCROLL_SPEED = 30
+
+--point in which the background will loop
+local BACKGROUND_LOOPING_POINT = 413
+
+local bird = Bird()
 
 function love.load()
-    love.graphics.setDefaultFilter('linear', 'linear')
+    love.graphics.setDefaultFilter('nearest', 'nearest')
 
     love.window.setTitle('Flappy Bird')
 
@@ -31,11 +47,21 @@ function love.keypressed(key)
     end
 end
 
+function love.update(dt)
+    backgroundScroll = (backgroundScroll + BACKGROUND_SCROLL_SPEED * dt) % BACKGROUND_LOOPING_POINT
+
+    groundScroll = (groundScroll + GROUND_SCROLL_SPEED * dt) % VIRTUAL_WIDTH
+end
+
 function love.draw()
     push:start()
 
-    love.graphics.draw(background, 0, 0)
+    love.graphics.draw(background, -backgroundScroll, 0)
 
-    love.graphics.draw(ground, 0, VIRTUAL_HEIGHT - 16)
+    love.graphics.draw(ground, -groundScroll, VIRTUAL_HEIGHT - 16)
+
+    --rendering bird
+    bird:render()
+
     push:finish()
 end
